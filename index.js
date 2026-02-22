ff38b0a33388284160a2d438e087fd3b48833a4f
justinli34 <justinlibc@gmail.com>
2026-02-21 17:39:36 -0800

    allow writing to empty directory

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
-import { cp, readFile, writeFile, rename, access } from "node:fs/promises";
-import { resolve } from "node:path";
+import { cp, readFile, writeFile, rename, access, readdir } from "node:fs/promises";
+import { resolve, basename } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const target = process.argv[2] ?? "my-app";
+const targetName = target === "." ? basename(process.cwd()) : target;
 const from = fileURLToPath(new URL("./template", import.meta.url));
 const to = resolve(process.cwd(), target);
 
 try {
   await access(to);
-  console.error(`Target already exists: ${to}`);
-  process.exit(1);
+  const entries = await readdir(to);
+  if (entries.length > 0) {
+    console.error(`Failed to create. Target directory is not empty: ${to}`);
+    process.exit(1);
+  }
 } catch (e) {
   if (e.code !== "ENOENT") throw e;
 }
@@ -25,10 +29,10 @@ const files = [
 
 for (const file of files) {
   const content = await readFile(file, "utf8");
-  await writeFile(file, content.replace(/vite-template/g, target));
+  await writeFile(file, content.replace(/vite-template/g, targetName));
 }
 
 const gitignore = resolve(to, "_gitignore");
 await rename(gitignore, resolve(to, ".gitignore"));
 
-console.log(`Created ${target}`);
+console.log(`Created ${targetName}`);
